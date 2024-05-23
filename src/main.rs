c4e2dd90c892df7608c5ede6487377f22b824f66
kkozoriz <kkozoriz7@yandex.ru>
2024-05-23 20:39:19 +0300

    redundant elements have been removed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use clap::Parser;
 use grep_rusty::Args;
-use std::{process, time};
+use std::process;
 fn main() {
-    let start = time::Instant::now();
     let args = Args::parse();
 
     if let Err(e) = grep_rusty::run(args) {
         eprintln!("Application error {e}");
         process::exit(1);
     }
-
-    let end = start.elapsed();
-    println!("Elapsed: {:.2?}", end);
 }
